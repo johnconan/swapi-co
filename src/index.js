f0dbad8cae510cfb3ab90b01c58dd7f6e2e1186a
Павел Хрусталев <Pavel@MacBook-Air-Pavel.local>
2019-01-12 17:08:36 +0300

    client-api

--- src/index.js
+++ src/index.js
@@ -1,31 +1,77 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-
-// const App = () => {
-
-//     return (
-//         <p>Hello</p>
-//     );
-// }
 
 
-// ReactDOM.render(<App />, document.getElementById('root'));
+class SwapiService {
 
-const getResourse = async (url) => {
-    const res = await fetch(url);
-    const body = await res.json();
-    return body;
+    _apiBase = 'https://swapi.co/api';
+
+    async getResourse(url) {
+        const res = await fetch(`${this._apiBase}${url}`);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url} 
+            received ${res.status}`);
+        }
+    
+        const body = await res.json();
+        return body;
+    }
+
+    async getAllPeople() {
+        const res = await this.getResourse(`/people/`);
+        return res.results;
+    }
+
+    getPerson(id) {
+        return this.getResourse(`/people/${id}`);
+    }
+
+    async getPlanets() {
+        const res = await this.getResourse(`/planets/`);
+        return res.results;
+    }
+
+    getPlanet(id) {
+        return this.getResourse(`/planets/${id}`);
+    }
+
+    async getStarships() {
+        const res = await this.getResourse(`/starships/`);
+        return res.results;
+    }
+    getStarship(id) {
+        return this.getResourse(`/starships/${id}`);
+    }
+
 }
 
-getResourse('https://swapi.co/api/people/1/')
-    .then((body) => {
-        console.log(body);
-    });
+const swapi = new SwapiService();
 
-// fetch ('https://swapi.co/api/people/1/')
-//     .then((res) => {
-//         return res.json();
-//     })
-//     .then((body) => {
-//         console.log(body);
-//     })
+swapi.getPerson(3).then((people) => {
+    console.log(people.name, '----Один Персонаж');
+})
+
+swapi.getAllPeople().then((people) => {
+    people.forEach(person => {
+        console.log(person.name);
+    });
+})
+
+swapi.getPlanets().then((planets) => {
+    planets.forEach((planet) => {
+        console.log(planet.name);
+    })
+})
+
+swapi.getPlanet(2).then((planet) => {
+    console.log(planet.name, '----одна планета');
+})
+
+swapi.getPlanets().then((starships) => {
+    starships.forEach((starship) => {
+        console.log(starship.name);
+    })
+})
+
+swapi.getPlanet(2).then((starship) => {
+    console.log(starship.name, '----один Корабль');
+})
