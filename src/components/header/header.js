d12789255f76befb284a9c431f484ede67653175
Павел Хрусталев <Pavel@MacBook-Air-Pavel.local>
2019-01-20 16:11:04 +0300

    music-commit

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,28 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './header.css';
 
-const Header = () => {
-  return (
-    <div className="header d-flex">
-      <h3>
-        <a href="#">
-          Star DB
-        </a>
-      </h3>
-      <ul className="d-flex">
-        <li>
-          <a href="#">People</a>
-        </li>
-        <li>
-          <a href="#">Planets</a>
-        </li>
-        <li>
-          <a href="#">Starships</a>
-        </li>
-      </ul>
-    </div>
-  );
+import starWarsSound from './zvezdnye-voyny-vstuplenie.mp3';
+
+class Header extends Component {
+
+  playMusic() {
+    const audio = new Audio(); 
+    audio.src = starWarsSound; 
+    audio.autoplay = true;
+  }
+
+  render() {
+    return (
+      <div className="header d-flex">
+        <h3>
+          <a href="#"
+            onClick={this.playMusic}>
+            Star Wars
+          </a>
+        </h3>
+        <ul className="d-flex">
+          <li>
+            <a href="#">People</a>
+          </li>
+          <li>
+            <a href="#">Planets</a>
+          </li>
+          <li>
+            <a href="#">Starships</a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 };
 
 export default Header;
